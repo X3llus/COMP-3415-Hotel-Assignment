9ff0ecefb9bd84bcf3e985ce2ada8959854fd8d7
X3llus <17010070+X3llus@users.noreply.github.com>
2021-11-18 18:19:44 -0500

    tailwind working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    mode: 'jit', // ⚠ Make sure to have this
-    purge: ["./src/**/*.svelte"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  mode: 'jit', // ⚠ Make sure to have this
+  purge: ["./src/**/*.svelte"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
