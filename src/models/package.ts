cf0dc4247ca5c33278f4685bf3f35ef036cb7eeb
Adam882094 <78448581+Adam882094@users.noreply.github.com>
2021-11-23 15:51:39 -0500

    Adding svelte page for package

--- src/models/package.ts
+++ src/models/package.ts
@@ -1,4 +1,4 @@
-import type { ObjectId } from "mongodb";
+import type { model, Schema, connect } from "mongodb";
 export default class Package {
     constructor(public breakfast: string, public holiday: string, public discount: string, 
                 public desc: string, public id?: ObjectId) {}
