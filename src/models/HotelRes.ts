d1412a99cbbb946fe5706a497371f752f563743b
Nicholas Santos <55286153+nicksantoscs@users.noreply.github.com>
2021-11-23 16:37:35 -0500

    Hotel reservation model

---
+++ src/models/HotelRes.ts
@@ -0,0 +1,62 @@
+import { model, Schema, connect } from 'mongoose';
+import type { IGuest } from './Guest';
+import dotenv from 'dotenv';
+dotenv.config()
+const uri = process.env['VITE_MONGO_URI'];
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+export interface HotelRes {
+    //id: IGuest
+    date: Date;
+    guestNb: number;
+    createHotelRes?(date: Date, guestNb: number): Promise<HotelRes>;
+    getHotelRes?(link: IGuest): Promise<HotelRes[]>;
+    updateHotelRes?(res: HotelRes): Promise<HotelRes>;
+    deleteHotelRes?(res: HotelRes): Promise<HotelRes>;
+}
+
+const HotelResSchema: Schema = new Schema({
+    date: { type: Date, required: true },
+    guestNb: {type: Number, required: true} 
+});
+
+HotelResSchema.methods.createHotelRes = async function ( date: Date, guestNb: number): Promise<HotelRes> {
+    await connect(uri), options ;
+
+    const res = new HotelResModel({
+        date: date,
+        guestNb: guestNb
+    });
+
+    // Creates and saves the id for the hotel reservation
+    const reservation: HotelRes = await res.save();
+    return reservation;
+}
+
+HotelResSchema.methods.getHotelRes = async function (link: IGuest): Promise<HotelRes[]> {
+    await connect(uri), options;
+
+    const reservation: HotelRes[] = await HotelResModel.find({
+        link:link
+    });
+    return reservation;
+}
+
+HotelResSchema.methods.updateHotelRes = async function ( res: HotelRes): Promise<HotelRes> {
+    await connect(uri), options;
+
+    const reservation: HotelRes = await HotelResModel.findOneAndUpdate(res, res);
+    return reservation;
+}
+
+HotelResSchema.methods.deleteHotelRes = async function (res: HotelRes): Promise<HotelRes> {
+    await connect(uri), options;
+
+    const reservation: HotelRes = await HotelResModel.findOneAndDelete(res);
+    return reservation;
+}
+
+export const HotelResModel = model<HotelRes>('HotelRes', HotelResSchema);
