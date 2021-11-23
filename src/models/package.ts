1633a2f1c22a6336d7161d11b643be4195c2680e
Adam882094 <78448581+Adam882094@users.noreply.github.com>
2021-11-23 16:22:30 -0500

    Added Schema

--- src/models/package.ts
+++ src/models/package.ts
@@ -3,12 +3,20 @@ import type { IGuest } from './Guest';
 import dotenv from 'dotenv';
 dotenv.config()
 const uri = process.env['VITE_MONGO_URI'];
+
 export declare interface Package{
     breakfast: string,
     holiday: string,
     discount: string,
     description: string,
 }
+
+export const roomSchema = new Schema({
+    breakfast: { type: String, required: true },
+    holiday: { type: String, required: true },
+    discount: { type: String, required: true },
+    description: { type: String, required: true },
+});
 // export default class Package {
 //     constructor(public breakfast: string, public holiday: string, public discount: string, 
 //                 public desc: string, public id?: connect) {}
