209070566a293b12ceea7ecacf94af60e76603d8
Adam882094 <78448581+Adam882094@users.noreply.github.com>
2021-11-23 16:18:29 -0500

    Updating package.ts

--- src/models/package.ts
+++ src/models/package.ts
@@ -1,5 +1,15 @@
-import type { model, Schema, connect } from "mongodb";
-export default class Package {
-    constructor(public breakfast: string, public holiday: string, public discount: string, 
-                public desc: string, public id?: ObjectId) {}
+import { Schema, model, connect } from 'mongoose';
+import type { IGuest } from './Guest';
+import dotenv from 'dotenv';
+dotenv.config()
+const uri = process.env['VITE_MONGO_URI'];
+export declare interface Package{
+    breakfast: string,
+    holiday: string,
+    discount: string,
+    description: string,
 }
+// export default class Package {
+//     constructor(public breakfast: string, public holiday: string, public discount: string, 
+//                 public desc: string, public id?: connect) {}
+// }
