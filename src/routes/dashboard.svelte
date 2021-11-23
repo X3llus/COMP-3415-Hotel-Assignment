0b014953720c619f0d9bf8b96285ffedbc68be18
Jonah <jonahjones9224@gmail.com>
2021-11-23 15:37:44 -0500

    Pushing good vibes

---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,72 @@
+<main class="container mt-12 mx-auto">
+    <div class="text-center">
+        <h1 class="text-5xl font-bold text-blue-900">
+            My Reservations
+        </h1>
+    </div>
+    <div class="bg-gray-200 rounded-3xl p-5 m-10">
+        <div class="p-5">
+            <div class="flex justify-between">
+                <h2 class="text-3xl font-bold">Hotel Reservations</h2>
+                <button class=" px-3 item text-white rounded-lg shadow bg-green-600 hover:bg-green-700 font-bold transition duration-200">
+                    <a href="#" class="">+ Reservation</a>
+                </button>
+            </div>
+            <div class="border-2 border-red-400 my-2"></div>
+            <div class="grid grid-cols-5 grid-wrap p-3 gap-4">
+                <!-- CARDS -->
+                <div class="card bg-white rounded-lg shadow-md p-2">
+                    <span class="text-lg font-bold">11/23/2021 - 11/25/2021</span>
+                    <div class="border border-red-400 "></div>
+                    <div class="px-2 py-1">
+                        <div>
+                            <span class="text-md font-bold">Guests: </span><span class="text-base font-normal">2</span>
+                        </div>
+                        <div>
+                            <span class="text-md font-bold">Package: </span><span class="text-base font-normal">Breakfast</span>
+                        </div>
+                        <div>
+                            <span class="text-md font-bold">Type: </span><span class="text-base font-normal">Lakefront Suite</span>
+                        </div>
+                        <div>
+                            <span class="text-md font-bold">Beds: </span><span class="text-base font-normal">2 Queens</span>
+                        </div>
+                        <div class="text-center mt-3">
+                            <button class="py-1 px-5 item text-white rounded-lg shadow bg-red-600 hover:bg-red-700 font-bold transition duration-200">
+                                <a href="#" class="">Cancel</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- CARDS END-->
+            </div>
+        </div>
+        <div class="p-5">
+            <div class="flex justify-between">
+                <h2 class="text-3xl font-bold">Restaurant Reservations</h2>
+                <button class=" px-3 item text-white rounded-lg shadow bg-green-600 hover:bg-green-700 font-bold transition duration-200">
+                    <a href="#" class="">+ Reservation</a>
+                </button>
+            </div>
+            <div class="border-2 border-red-400 my-2"></div>
+            <div class="grid grid-cols-5 grid-wrap p-3 gap-4">
+                <!-- CARDS -->
+                <div class="card bg-white rounded-lg shadow-md p-2">
+                    <span class="text-lg font-bold">11/23/2021 - 11:45</span>
+                    <div class="border border-red-400 "></div>
+                    <div class="px-2 py-1">
+                        <div>
+                            <span class="text-md font-bold">Guests: </span><span class="text-base font-normal">2</span>
+                        </div>
+                        <div>
+                        <div class="text-center mt-3">
+                            <button class="py-1 px-5 item text-white rounded-lg shadow bg-red-600 hover:bg-red-700 font-bold transition duration-200">
+                                <a href="#" class="">Cancel</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- CARDS END-->
+        </div>
+    </div>  
+</main>
---
+++ src/routes/mDashboard.svelte
@@ -0,0 +1,17 @@
+<main class="container mt-12 mx-auto justify-center">
+    <div class="text-center m-5">
+        <h1 class="text-5xl font-bold text-blue-900">Hotel Dashboard</h1>
+    </div>
+    <div class="flex justify-center">
+        <div class="bg-gray-100 rounded-md inline-flex">
+                <a href="#" class="py-1.5 px-2 block text-gray-600 font-bold">Hotel Reservations</a>
+                <div class="border-l-2"></div>
+                <a href="#" class="py-1.5 px-2 block text-gray-600 font-bold">Resturant Reservations</a>
+                <div class="border-l-2"></div>
+                <a href="#" class="py-1.5 px-2 block text-gray-600 font-bold">Rooms</a>
+                <div class="border-l-2"></div>
+                <a href="#" class="py-1.5 px-2 block text-gray-600 font-bold">Guests</a>
+        </div>
+    </div>
+    <!-- TABLES-->
+</main>
