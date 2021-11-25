d84076468219acfba04cac93bb64ef5d466a5bc1
X3llus <17010070+X3llus@users.noreply.github.com>
2021-11-24 22:55:05 -0500

    moved getting user from token to function

--- src/routes/api/resturantRes.ts
+++ src/routes/api/resturantRes.ts
@@ -1,5 +1,6 @@
 import type { Guest } from '$models/Guest';
 import { ResturantResModel, IResturantRes } from '../../models/ResturantRes';
+import { token } from './user/token';
 
 interface Body {
     guest: Guest;
