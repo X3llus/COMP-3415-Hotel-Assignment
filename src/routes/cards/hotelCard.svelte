ebbf08af3eab1288db2b70c35e31b01cc79396d1
Jonah <jonahjones9224@gmail.com>
2021-11-25 13:02:14 -0500

    hotelCard

---
+++ src/routes/cards/hotelCard.svelte
@@ -0,0 +1,27 @@
+<script>
+    
+</script>
+
+<div class="card bg-white rounded-lg shadow-md p-2">
+    <span class="text-lg font-bold">11/23/2021 - 11/25/2021</span>
+    <div class="border border-red-400 "></div>
+    <div class="px-2 py-1">
+        <div>
+            <span class="text-md font-bold">Guests: </span><span class="text-base font-normal">2</span>
+        </div>
+        <div>
+            <span class="text-md font-bold">Package: </span><span class="text-base font-normal">Breakfast</span>
+        </div>
+        <div>
+            <span class="text-md font-bold">Type: </span><span class="text-base font-normal">Lakefront Suite</span>
+        </div>
+        <div>
+            <span class="text-md font-bold">Beds: </span><span class="text-base font-normal">2 Queens</span>
+        </div>
+        <div class="text-center mt-3">
+            <button class="py-1 px-5 item text-white rounded-lg shadow bg-red-600 hover:bg-red-700 font-bold transition duration-200">
+                <a href="#" class="">Cancel</a>
+            </button>
+        </div>
+    </div>
+</div>
